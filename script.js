7ef326d04ae3f0a4e3b20bff7344c3496d59b510
Kamri Larsen <kamri.larsen@gmail.com>
2019-05-29 22:08:00 -0600

    functioning thesaurus, updated style

--- script.js
+++ script.js
@@ -6,23 +6,38 @@ let app = new Vue({
         loading: false,
         searchEntry: '',
         definitionList: [],
+        thesaurus: [],
+        thesError: '.',
     },
     methods: {
         async searchWord() {
             try {
                 this.definitionList = [];
+                this.thesaurus = [];
+                this.thesError = '.';
+                if (this.searchEntry === '')
+                {
+                    return;
+                }
                 this.loading = true;
                 const response = await axios.get('https://www.dictionaryapi.com/api/v3/references/collegiate/json/' + this.searchEntry + '?key=d33ff619-479e-42ba-ad0d-f1f577b0e4df');
                 console.log(response);
+                var tempError = '';
                 for (i in response.data)
                 {
-                    var tempDate = '';
-                    var tempSpeechPart = '';
+                    var tempDate = 'Unknown';
+                    var tempSpeechPart = 'Unknown';
                     var tempOffensive = '';
                     var tempShortDefs = '';
+                    var tempName = '';
 
                     if (response.data[i].date !== undefined) {
                         tempDate = response.data[i].date;
+                        var pos = tempDate.indexOf("{");
+                        if (pos !== -1) {
+                            var res = tempDate.slice(0, pos);
+                            tempDate = res;
+                        }
                     }
                     if (response.data[i].fl !== undefined) {
                         tempSpeechPart = response.data[i].fl;
@@ -32,37 +47,94 @@ let app = new Vue({
                     }
                     if (response.data[i].shortdef !== undefined) {
                         tempShortDefs = response.data[i].shortdef;
+                        var letters = /^[A-Za-z]+$/;
+                        for (j in tempShortDefs) {
+                            if(!tempShortDefs[j].charAt(0).match(letters))
+                            {
+                                tempShortDefs[j] = tempShortDefs[j].slice(1);
+                            }
+                        }
                     }
 
-                    if ((tempDate === '') && (tempOffensive === '') && (tempSpeechPart === '') && (tempShortDefs === '')) {
-                        tempDate = "We're sorry, that word cannot be defined. Did you mean to type one of the following words?";
-                        tempShortDefs = response.data;
+                    if ((response.data[i].meta !== undefined) && (response.data[i].meta.id !== undefined)) {
+                        tempName = response.data[i].meta.id;
+                        var pos = tempName.indexOf(":");
+                        if (pos !== -1) {
+                            var res = tempName.slice(0, pos);
+                            tempName = res;
+                        }
+                    }
 
+                    if ((tempDate === 'Unknown') && (tempOffensive === '') && (tempSpeechPart === 'Unknown') && (tempShortDefs === '')) {
                         this.definitionList.push({
-                            date: tempDate,
-                            speechPart: tempSpeechPart,
-                            offensive: tempOffensive,
-                            shortDefs: tempShortDefs,
+                            error: "We're sorry, that word cannot be defined. Did you mean to type one of the following words?",
+                            suggestions: response.data,
                         })
-
                         break;
                     }
+
+                    this.thesError = '';
 
                     this.definitionList.push({
                         date: tempDate,
                         speechPart: tempSpeechPart,
                         offensive: tempOffensive,
                         shortDefs: tempShortDefs,
+                        word: tempName,
+                        error: tempError,
+                        suggestions: [],
                     });
-                    
-                    //Fix dates by checking for {} type of info at the end
-                    //Fix if bolded statements show up based on if the category is empty
-                    //Add thesaurus functionality or something in the side bar
-                    //Get rid of bullet points on the lists - change them to something else maybe
-                    //Add button to filter words that can be defined based on if offensive
-                    //Obviously stylize stuff - better fonts, spacing, colors
-                
                 }
+                const responseth = await axios.get('https://www.dictionaryapi.com/api/v3/references/thesaurus/json/' + this.searchEntry + '?key=ddc164f6-e086-4fda-898a-76cd24669771');
+                console.log(responseth);
+                for (i in responseth.data)
+                {
+                    var tempSyns = [];
+                    var tempAnts = [];
+                    var tempName = '';
+                    var tempSpeechPart = 'Unknown';
+                    
+                    if ((responseth.data[i].meta !== undefined) && (responseth.data[i].meta.id !== undefined)) {
+                        tempName = responseth.data[i].meta.id;
+                        var pos = tempName.indexOf(":");
+                        if (pos !== -1) {
+                            var res = tempName.slice(0, pos);
+                            tempName = res;
+                        }
+                    }
+                    
+                    if (responseth.data[i].fl !== undefined) {
+                        tempSpeechPart = responseth.data[i].fl;
+                    }
+
+                    if ((responseth.data[i].meta !== undefined) && (responseth.data[i].meta.ants !== undefined))
+                    {
+                        for (k in responseth.data[i].meta.ants[0]) {
+                            tempAnts.push(responseth.data[i].meta.ants[0][k]);
+                        }
+                    }
+
+                    if ((responseth.data[i].meta !== undefined) && (responseth.data[i].meta.syns !== undefined))
+                    {
+                        for (k in responseth.data[i].meta.syns[0]) {
+                            tempSyns.push(responseth.data[i].meta.syns[0][k]);
+                        }
+                    }
+
+                    if ((tempAnts.length == 0) && (tempSpeechPart === 'Unknown') && (tempSyns.length == 0)) { 
+                        break;
+                    }
+
+                    this.thesaurus.push({
+                        speechPart: tempSpeechPart,
+                        word: tempName,
+                        Ants: tempAnts,
+                        Syns: tempSyns,
+                        error: tempError,
+                    });
+
+                }
+
                 this.loading = false;
                 searchEntry = '';
             } catch (error) {
